a17735d8b64f3b53a3c0cf203dc823271a1d08b2
Phil Knowles <fighters4christ@outlook.com>
2023-11-12 18:24:03 -0600

    Updates

---
+++ precache-manifest.a5f88edfff2bf72337b88a49c3409897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e50fd7ce5d5c8b0f8c82d80b07c9377",
+    "url": "/your-repo-name/index.html"
+  },
+  {
+    "revision": "6823e1e17ac14e40a9ae",
+    "url": "/your-repo-name/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "01f3d01e20ca615e43a9",
+    "url": "/your-repo-name/static/js/2.8a6596eb.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/your-repo-name/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "6823e1e17ac14e40a9ae",
+    "url": "/your-repo-name/static/js/main.eed73d4c.chunk.js"
+  },
+  {
+    "revision": "f6ee0d0d824ad8520aca",
+    "url": "/your-repo-name/static/js/runtime~main.294dea45.js"
+  }
+]);
