af3ac2c36fad28c3176eff0974389054188da324
Phil Knowles <fighters4christ@outlook.com>
2023-11-13 08:30:57 -0600

    Updates

---
+++ precache-manifest.1dc585d449026e3e51df85fc38275071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f573e3e89ecb3fc6597d17d7fb58802b",
+    "url": "/yellowbookbag/index.html"
+  },
+  {
+    "revision": "8d452dbe954e6bc46b01",
+    "url": "/yellowbookbag/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "01f3d01e20ca615e43a9",
+    "url": "/yellowbookbag/static/js/2.8a6596eb.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/yellowbookbag/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "8d452dbe954e6bc46b01",
+    "url": "/yellowbookbag/static/js/main.4fad9d22.chunk.js"
+  },
+  {
+    "revision": "a86ae4f6a8508ac7e79b",
+    "url": "/yellowbookbag/static/js/runtime~main.2bbb323d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/your-repo-name/precache-manifest.a95c842abd7bc94b102dd8b249e35297.js"
+  "/yellowbookbag/precache-manifest.1dc585d449026e3e51df85fc38275071.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/your-repo-name/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yellowbookbag/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
