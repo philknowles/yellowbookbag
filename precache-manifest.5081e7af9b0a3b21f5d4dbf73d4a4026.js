bf8a5c72aa3760475b1c2cbea1db24571c0672f6
Phil Knowles <fighters4christ@outlook.com>
2023-11-13 21:52:09 -0600

    Updates

--- precache-manifest.5081e7af9b0a3b21f5d4dbf73d4a4026.js
+++ precache-manifest.5081e7af9b0a3b21f5d4dbf73d4a4026.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f573e3e89ecb3fc6597d17d7fb58802b",
+    "revision": "c29e6e33f84064703fc5cf0508d1b6e4",
     "url": "/yellowbookbag/index.html"
   },
   {
-    "revision": "8d452dbe954e6bc46b01",
+    "revision": "4e875044c0a28a06f780",
     "url": "/yellowbookbag/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "01f3d01e20ca615e43a9",
-    "url": "/yellowbookbag/static/js/2.8a6596eb.chunk.js"
+    "revision": "87e6c5c472c96a5f6ce8",
+    "url": "/yellowbookbag/static/js/2.43059f4f.chunk.js"
   },
   {
     "revision": "2c10491364e9bdf48063",
     "url": "/yellowbookbag/static/js/3.fc5cbf83.chunk.js"
   },
   {
-    "revision": "8d452dbe954e6bc46b01",
-    "url": "/yellowbookbag/static/js/main.4fad9d22.chunk.js"
+    "revision": "4e875044c0a28a06f780",
+    "url": "/yellowbookbag/static/js/main.0190c50e.chunk.js"
   },
   {
     "revision": "a86ae4f6a8508ac7e79b",
