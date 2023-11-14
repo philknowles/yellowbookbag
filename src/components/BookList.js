65869038b03790e628d3860c8edea26237d0755e
Phil Knowles <fighters4christ@outlook.com>
2023-11-13 21:51:35 -0600

    Added pagination

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -6,6 +6,8 @@ const BookList = () => {
   const [books, setBooks] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedBook, setSelectedBook] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5); // Adjust the number of items per page as needed
 
   const fetchData = async () => {
     try {
@@ -13,6 +15,7 @@ const BookList = () => {
         `https://openlibrary.org/search.json?q=${searchTerm}`
       );
       setBooks(response.data.docs);
+      setCurrentPage(1); // Reset to the first page when a new search is performed
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -34,19 +37,27 @@ const BookList = () => {
     setSelectedBook(null);
   };
 
+  const indexOfLastBook = currentPage * itemsPerPage;
+  const indexOfFirstBook = indexOfLastBook - itemsPerPage;
+  const currentBooks = books.slice(indexOfFirstBook, indexOfLastBook);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
       <h2>Search Books</h2>
       <input
         type="text"
         id="searchInput"
-        placeholder="Search by title..."
+        placeholder="Enter your search query"
         value={searchTerm}
         onChange={handleSearch}
       />
       <button onClick={handleSearchButtonClick}>Search</button>
       <ul>
-        {books.map((book) => (
+        {currentBooks.map((book) => (
           <li key={book.key}>
             <h3>{book.title}</h3>
             {book.cover_i && (
@@ -61,6 +72,25 @@ const BookList = () => {
           </li>
         ))}
       </ul>
+      {books.length > itemsPerPage && (
+        <nav>
+          <ul className="pagination">
+            {Array.from({ length: Math.ceil(books.length / itemsPerPage) }).map(
+              (_, index) => (
+                <li
+                  key={index + 1}
+                  className={currentPage === index + 1 ? 'active' : ''}
+                  onClick={() => paginate(index + 1)}
+                >
+                  <a href="#!" onClick={() => paginate(index + 1)}>
+                    {index + 1}
+                  </a>
+                </li>
+              )
+            )}
+          </ul>
+        </nav>
+      )}
       {selectedBook && (
         <div>
           <BookDetails book={selectedBook} />
