843f69312fa4dd2dd6f6d04555b7784f9dd1113c
Phil Knowles <fighters4christ@outlook.com>
2023-11-19 14:22:48 -0600

    Updates

---
+++ precache-manifest.d3605c62abe6f7ef31567df71079b5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72a08ebe152e4058b4c4dfd74125c12",
+    "url": "/yellowbookbag/index.html"
+  },
+  {
+    "revision": "904c922620062923dc34",
+    "url": "/yellowbookbag/static/css/main.198d35e2.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/yellowbookbag/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/yellowbookbag/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "904c922620062923dc34",
+    "url": "/yellowbookbag/static/js/main.4daf1b1c.chunk.js"
+  },
+  {
+    "revision": "e33161c8612df29ba3ee",
+    "url": "/yellowbookbag/static/js/runtime~main.2ef8ab6f.js"
+  }
+]);
