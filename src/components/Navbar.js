539eb1269f7d31e924b7b92fd02add48383cc82c
Phil Knowles <fighters4christ@outlook.com>
2023-11-24 15:01:42 -0600

    Working scanner input field (#10)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,11 @@ const Navbar = ({ searchTerm, onSearchChange, onSearchSubmit }) => {
   const navigate = useNavigate();
   const [scannedBarcode, setScannedBarcode] = useState('');
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [showVideo, setShowVideo] = useState(false);
+  const [barcodeCaptured, setBarcodeCaptured] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasSearched, setHasSearched] = useState(false);
+
 
   useEffect(() => {
     const handleResize = () => {
@@ -22,60 +27,105 @@ const Navbar = ({ searchTerm, onSearchChange, onSearchSubmit }) => {
 
   const handleBarcodeScanned = (barcode) => {
     setScannedBarcode(barcode);
+    setBarcodeCaptured(true);
+    setShowVideo(false);
   };
 
   const handleSearchInApp = () => {
     console.log('Search in app for ISBN:', scannedBarcode);
-    // Perform any additional logic, such as searching within your application
+    const query = `https://openlibrary.org/search.json?q=${scannedBarcode}`;
+
+    // Fetch the data from the API
+    fetch(query)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setBooks(data.docs);
+        setHasSearched(true);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error.message);
+        // Handle the error, e.g., display a user-friendly message
+      });
+  };
+
+  const handleSearchFieldClick = () => {
+    setShowVideo(true);
+    setBarcodeCaptured(false);
+  };
+
+  const handleVideoClose = () => {
+    setShowVideo(false);
+    setBarcodeCaptured(false);
   };
 
   const currentURL = window.location.pathname;
 
   return (
-    <nav className="navbar">
-      <div className="navbar-title">
-        <h2>The Yellow Book Bag</h2>
-      </div>
-      <div className="navbar-search">
-        <ul className="navbar-list">
-          <li>
-            <button onClick={() => navigate('/')}>Home</button>
-          </li>
-          <li>
-            <button onClick={() => navigate('/about')}>About</button>
-          </li>
-        </ul>
-        {currentURL === '/' || currentURL === '/yellowbookbag/' ? (
-          isMobile ? (
-            <div>
-              <BarcodeScanner onBarcodeScanned={handleBarcodeScanned} />
-              <input
-                type="text"
-                value={scannedBarcode}
-                placeholder="Enter ISBN..."
-                onChange={() => {}}
-              />
-              <button onClick={handleSearchInApp}>
-                <i className="fas fa-search"></i> {/* Magnifying glass icon */}
-              </button>
-            </div>
-          ) : (
-            <>
-              <input
-                type="text"
-                id="searchInput"
-                placeholder="Enter your search query"
-                value={searchTerm}
-                onChange={onSearchChange}
-              />
-              <button onClick={onSearchSubmit}>
-                <i className="fas fa-search"></i> {/* Magnifying glass icon */}
-              </button>
-            </>
-          )
-        ) : null}
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="navbar-title">
+          <h2>The Yellow Book Bag</h2>
+        </div>
+        <div className="navbar-search">
+          <ul className="navbar-list">
+            <li>
+              <button onClick={() => navigate('/')}>Home</button>
+            </li>
+            <li>
+              <button onClick={() => navigate('/about')}>About</button>
+            </li>
+          </ul>
+          {currentURL === '/' || currentURL === '/yellowbookbag/' ? (
+            isMobile ? (
+              <>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Scan ISBN..."
+                    onChange={onSearchChange}
+                    onClick={handleSearchFieldClick}
+                  />
+                  {showVideo && (
+                    <div className="video-container">
+                      <BarcodeScanner onBarcodeScanned={handleBarcodeScanned} />
+                      <button className="close-video-btn" onClick={handleVideoClose}>
+                        <i className="fas fa-times"></i> {/* Close icon */}
+                      </button>
+                    </div>
+                  )}
+                  <button onClick={onSearchSubmit}>
+                    <i className="fas fa-search"></i> {/* Magnifying glass icon */}
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <input
+                  type="text"
+                  id="searchInput"
+                  placeholder="Enter your search query"
+                  value={searchTerm}
+                  onChange={onSearchChange}
+                />
+                <button onClick={onSearchSubmit}>
+                  <i className="fas fa-search"></i> {/* Magnifying glass icon */}
+                </button>
+              </>
+            )
+          ) : null}
+        </div>
+      </nav>
+      {showVideo && !barcodeCaptured && (
+        <div className="video-container">
+          <BarcodeScanner onBarcodeScanned={handleBarcodeScanned} />
+        </div>
+      )}
+    </>
   );
 };
 
