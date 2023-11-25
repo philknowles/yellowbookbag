17dd2e0e002b310e70f7fb7b74d4824d71d3e3f1
Phil Knowles <fighters4christ@outlook.com>
2023-11-25 16:59:11 -0600

    Consolidated the search and hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+// Navbar.js
+
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
@@ -8,8 +10,7 @@ const Navbar = ({ searchTerm, onSearchChange, onSearchSubmit }) => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const [showVideo, setShowVideo] = useState(false);
   const [barcodeCaptured, setBarcodeCaptured] = useState(false);
-  const [books, setBooks] = useState([]);
-  const [hasSearched, setHasSearched] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -24,31 +25,9 @@ const Navbar = ({ searchTerm, onSearchChange, onSearchSubmit }) => {
   }, []);
 
   const handleBarcodeScanned = (barcode) => {
-    console.log('Scanned barcode:', barcode);
     setBarcodeCaptured(true);
     setShowVideo(false);
-    onSearchChange({ target: { value: barcode } }); // Update searchTerm with scanned barcode
-  };
-
-  const handleSearchInApp = () => {
-    console.log('Search in app for ISBN:', searchTerm);
-    const query = `https://openlibrary.org/search.json?q=${searchTerm}`;
-
-    // Fetch the data from the API
-    fetch(query)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        onSearchSubmit(); // Call the provided onSearchSubmit prop
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error.message);
-        // Handle the error, e.g., display a user-friendly message
-      });
+    onSearchChange({ target: { value: barcode } });
   };
 
   const handleSearchFieldClick = () => {
@@ -61,62 +40,77 @@ const Navbar = ({ searchTerm, onSearchChange, onSearchSubmit }) => {
     setBarcodeCaptured(false);
   };
 
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleNavigate = (path) => {
+    setMenuOpen(false);
+    navigate(path);
+  };
+
   const currentURL = window.location.pathname;
 
   return (
     <>
-      <nav className="navbar">
+      <nav className={`navbar ${menuOpen ? 'menu-open' : ''}`}>
         <div className="navbar-title">
           <Link to="/">
-              <h2>The Yellow Book Bag</h2>
+            <h2>The Yellow Book Bag</h2>
           </Link>
         </div>
-        <div className="navbar-search">
-          <ul className="navbar-list">
-            <li>
-              <button onClick={() => navigate('/about')}>About</button>
-            </li>
-          </ul>
-          {currentURL === '/' || currentURL === '/yellowbookbag/' ? (
-            isMobile ? (
-              <>
-                <div>
+        <div className="navbar-search-container">
+          <div className="navbar-search">
+            {currentURL === '/' || currentURL === '/yellowbookbag/' ? (
+              isMobile ? (
+                <>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Scan ISBN..."
+                      value={searchTerm}
+                      onChange={onSearchChange}
+                      onClick={handleSearchFieldClick}
+                    />
+                    {showVideo && (
+                      <div className="video-container">
+                        <BarcodeScanner onBarcodeScanned={handleBarcodeScanned} />
+                        <button className="close-video-btn" onClick={handleVideoClose}>
+                          <i className="fas fa-times"></i>
+                        </button>
+                      </div>
+                    )}
+                    <button className="search-button" onClick={onSearchSubmit}>
+                      <i className="fas fa-search"></i>
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <>
                   <input
                     type="text"
-                    placeholder="Scan ISBN..."
+                    id="searchInput"
+                    placeholder="Enter your search query"
                     value={searchTerm}
                     onChange={onSearchChange}
-                    onClick={handleSearchFieldClick}
                   />
-                  {showVideo && (
-                    <div className="video-container">
-                      {/* Assuming BarcodeScanner component works as expected */}
-                      <BarcodeScanner onBarcodeScanned={handleBarcodeScanned} />
-                      <button className="close-video-btn" onClick={handleVideoClose}>
-                        <i className="fas fa-times"></i>
-                      </button>
-                    </div>
-                  )}
-                  <button onClick={onSearchSubmit}>
+                  <button className="search-button" onClick={onSearchSubmit}>
                     <i className="fas fa-search"></i>
                   </button>
-                </div>
-              </>
-            ) : (
-              <>
-                <input
-                  type="text"
-                  id="searchInput"
-                  placeholder="Enter your search query"
-                  value={searchTerm}
-                  onChange={onSearchChange}
-                />
-                <button onClick={onSearchSubmit}>
-                  <i className="fas fa-search"></i>
-                </button>
-              </>
-            )
-          ) : null}
+                </>
+              )
+            ) : null}
+          </div>
+          <div className="navbar-list">
+            <button className="menu-toggle" onClick={handleMenuToggle}>
+              <i className={`fas ${menuOpen ? 'fa-times' : 'fa-bars'}`}></i>
+            </button>
+            {menuOpen && (
+              <ul>
+                <li onClick={() => handleNavigate('/about')}>About</li>
+              </ul>
+            )}
+          </div>
         </div>
       </nav>
     </>
